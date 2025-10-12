7ebc4ff093fc18f52b0cb7957dde71ed0b245abf
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2025-10-12 11:51:36 -0400

    clicking a folder now shows content inside

---
+++ public/scripts/uploaderScripts.js
@@ -0,0 +1,39 @@
+//uploaderScripts.js
+
+// folder onClick function
+function openFolder(folderId) {
+  console.log("oppening folder", folderId);
+  // store current folderId in window
+  window.history.pushState({ folderId }, "", window.location.pathname);
+
+  // close all folders
+  closeAllFolders();
+
+  // add open class to current folder
+  let folderElement = document.getElementById("folder" + folderId);
+  folderElement.classList.add("open");
+
+  // Show contents
+  showFolderContent(folderId);
+}
+
+// close all folders
+function closeAllFolders() {
+  let allFolders = document.querySelectorAll(".folder");
+  allFolders.forEach((folder) => {
+    folder.classList.remove("open");
+  });
+
+  let allContents = document.querySelectorAll(".folderContent");
+  allContents.forEach((content) => {
+    content.style.display = "none";
+  });
+}
+
+// show folder content
+function showFolderContent(folderId) {
+  let folderContent = document.getElementById("content" + folderId);
+  if (folderContent) {
+    folderContent.style.display = "block";
+  }
+}
